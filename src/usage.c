0d9f7bdb2cf8add40c36d8c0bc888e05c347eb78
Nickolas Burr <nickolasburr@gmail.com>
2017-08-04 19:48:08 -0400

    Add usage.{c,h} to repository

---
+++ src/usage.c
@@ -0,0 +1,14 @@
+/**
+ * usage.c
+ *
+ * Copyright (C) 2017 Nickolas Burr <nickolasburr@gmail.com>
+ */
+
+#include "usage.h"
+
+/**
+ * Print formatted usage message
+ */
+void pfusage () {
+	printf("%s\n", GIT_STASHD_USAGE_MESSAGE);
+}
