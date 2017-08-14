16dfd09dcb1b0a0b118e46176246d8c2c4e0b3ef
Nickolas Burr <nickolasburr@gmail.com>
2017-08-14 13:30:53 -0400

    Work on memory allocation for repo struct

--- src/repo.c
+++ src/repo.c
@@ -9,20 +9,41 @@
 /**
  * Get all stashes for a repository.
  */
-struct stash *get_stashes (struct repo *repo) {
-	return repo->stashes;
+struct stash *get_stashes (struct repo *r) {
+	return r->stashes;
 }
 
 /**
  * Set a copy of all stashes for a repository in memory.
  */
-void set_stashes (struct repo *repo) {
+void set_stashes (struct repo *r) {
 	// @todo: Build this function out.
+	char *cmd;
+	FILE *fp;
+
+	printf("set_stashes -> r->path -> %s\n", r->path);
+
+	sprintf(cmd, "/usr/bin/git -C %s -- stash list", r->path);
+
+	fp = popen(cmd, "r");
+
+	if (!fp) {
+		printf("Could not open pipe!\n");
+
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(r->stashes->entries, (sizeof(r->stashes->entries) - 1), fp) != NULL) {
+		printf("%s\n", r->stashes->entries);
+	}
+
+	pclose(fp);
 }
+
 
 /**
  * Check if the worktree has changed since our last check.
  */
-int has_worktree_changed (struct repo *repo) {
+int has_worktree_changed (struct repo *r) {
 	return 1;
 }
