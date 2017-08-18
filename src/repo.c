17d53a1d40b1d15f08a80f9f03dd3922156d9fb6
Nickolas Burr <nickolasburr@gmail.com>
2017-08-18 00:10:57 -0400

    Work on stash retrieval, memory allocation

--- src/repo.c
+++ src/repo.c
@@ -3,27 +3,27 @@
  *
  * Copyright (C) 2017 Nickolas Burr <nickolasburr@gmail.com>
  */
-
 #include "repo.h"
 
 /**
- * Get all stashes for a repository.
+ * Get all stash entries for a repository.
  */
-struct stash *get_stashes (struct repo *r) {
-	return r->stashes;
+struct stash *get_stash (struct repo *r) {
+	return r->stash;
 }
 
 /**
- * Set a copy of all stashes for a repository in memory.
+ * Set a copy of all stash entries for a repository.
  */
-void set_stashes (struct repo *r) {
-	// @todo: Build this function out.
+void set_stash (struct repo *r) {
 	char *cmd;
 	FILE *fp;
 
-	printf("set_stashes -> r->path -> %s\n", r->path);
+	// Allocate space for command string
+	cmd = ALLOC(sizeof(char) * (strlen(r->path) + 100));
 
-	sprintf(cmd, "/usr/bin/git -C %s -- stash list", r->path);
+	sprintf(cmd, "/usr/bin/git -C %s stash list", r->path);
+	printf("set_stashes -> cmd -> %s\n", cmd);
 
 	fp = popen(cmd, "r");
 
@@ -33,16 +33,18 @@ void set_stashes (struct repo *r) {
 		exit(EXIT_FAILURE);
 	}
 
-	while (fgets(r->stashes->entries, (sizeof(r->stashes->entries) - 1), fp) != NULL) {
-		printf("%s\n", r->stashes->entries);
+	while (fgets(r->stash->entries, (sizeof(r->stash->entries) - 1), fp) != NULL) {
+		printf("%s\n", r->stash->entries);
 	}
 
+	// Free space allocated for commnad string
+	FREE(cmd);
 	pclose(fp);
 }
 
 
 /**
- * Check if the worktree has changed since our last check.
+ * Check if the worktree has changed since the last check.
  */
 int has_worktree_changed (struct repo *r) {
 	return 1;
