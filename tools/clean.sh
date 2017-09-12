f305c4d1ad85e08d092c84a857e20ffcbda6c247
Nickolas Burr <nickolasburr@gmail.com>
2017-09-12 13:40:02 -0400

    Remove log files during make clean

--- tools/clean.sh
+++ tools/clean.sh
@@ -13,6 +13,10 @@ if test -f "$PROJ_DIR/$TARGET"; then
 	rm -rf "$PROJ_DIR/$TARGET"
 fi
 
+if test -f "$PROJ_DIR/$TARGET.log"; then
+	rm -f "$PROJ_DIR/$TARGET.log"
+fi
+
 if test -d "$PROJ_DIR/$TARGET.dSYM"; then
 	rm -rf "$PROJ_DIR/$TARGET.dSYM"
 fi
