5d12d6f5d0cd4bf487b2aa222ebeaf05d124b755
Nickolas Burr <nickolasburr@gmail.com>
2017-09-29 17:24:04 -0400

    Build get_git_dir function

--- include/git.h
+++ include/git.h
@@ -46,8 +46,9 @@ struct git_stashd_repository {
 };
 
 void add_stash_entry(int *error, const char *path, struct git_stashd_stash *s);
+char *get_git_dir(int*, const char*);
 int has_coequal_entry(int *error, const char *path, struct git_stashd_stash *s);
-int has_stashd_lock(int *error, const char *path);
+int has_lock(int *error, const char *path);
 int is_worktree_dirty(int *error, const char *path);
 
 git_stash_cb *init_setup(size_t index, const char *message, const git_oid *stash_id, void *payload);
